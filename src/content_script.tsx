fb0a3c3c683b9229c43dd3fef51ff407b64e3c15
“谢俊彦” <junyan.xie@hollyland-tech.com>
2024-11-22 14:40:41 +0800

    实现划词翻译

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,3 +1,5 @@
+import { Panel } from './panel';
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.color) {
     console.log("Receive color = " + msg.color);
@@ -7,3 +9,29 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse("Color message is none.");
   }
 });
+
+
+
+
+
+
+let panel: Panel = new Panel();
+console.log("执行到这");
+
+window.onmouseup = function (e: MouseEvent): void {
+  let selection = window.getSelection();
+  if (!selection ) {
+    return;
+  }
+  let raw: string = selection.toString().trim();
+  let x: number = e.pageX;
+  let y: number = e.pageY;
+
+  if (!raw) {
+    return;
+  } else {
+    panel.pos({ x: x, y: y });
+    panel.translate(raw);
+    panel.show();
+  }
+}
---
+++ src/panel.ts
@@ -0,0 +1,74 @@
+export class Panel {
+    private container!: HTMLDivElement;
+    private close!: HTMLElement;
+    private source!: HTMLElement;
+    private dest!: HTMLElement;
+
+    constructor() {
+        this.create();
+        this.bind();
+    }
+
+    create(): void {
+        let container: HTMLDivElement = document.createElement('div');
+        console.log("create 函数执行");
+
+        let html: string = `
+            <header>翻译<span class="close">X</span></header>
+            <main>
+                <div class="source">
+                <div class="title">英语</div>
+                <div class="content"></div>
+                </div>
+                <div class="dest">
+                <div class="title">简体中文</div>
+                <div class="content">...</div>
+                </div>
+            </main>
+        `;
+
+        container.innerHTML = html;
+        container.classList.add('translate-panel');
+        document.body.appendChild(container);
+
+        this.container = container;
+        this.close = container.querySelector('.close') as HTMLElement;
+        this.source = container.querySelector('.source .content') as HTMLElement;
+        this.dest = container.querySelector('.dest .content') as HTMLElement;
+    }
+
+    show(): void {
+        this.container.classList.add('show');
+    }
+
+    hide(): void {
+        this.container.classList.remove('show');
+    }
+
+    bind(): void {
+        this.close.onclick = () => {
+            this.hide();
+        };
+    }
+
+    translate(raw: string): void {
+        this.source.innerText = raw;
+        this.dest.innerText = '...';
+
+        let slValue: string = 'en';
+        let tlValue: string = 'zh-Hans';
+
+        fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${slValue}&tl=${tlValue}&dt=t&q=${raw}`)
+            .then(res => res.json())
+            .then(res => {
+                this.dest.innerText = res[0][0][0];
+                console.log("翻译为" + res[0][0][0]);
+            });
+    }
+
+    pos(pos: { x: number; y: number }): void {
+        this.container.style.top = pos.y + 'px';
+        this.container.style.left = pos.x + 'px';
+    }
+}
+
